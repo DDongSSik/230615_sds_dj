95252ebbebbb676a1f658cc0946fbf523ef16063
DDongSSik <118751809+DDongSSik@users.noreply.github.com>
2023-06-20 20:13:29 +0900

    프로젝트 이동 추가
    애니메이션 구현

---
+++ 230615_sds/Assets/Scripts/ObjectMove.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMove : MonoBehaviour
+{
+    int step = 0;
+   
+    void Update(){
+        float x = this.transform.position.x;
+        float y = this.transform.position.y;
+    }
+}
