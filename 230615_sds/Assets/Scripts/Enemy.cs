bc0b65f591f5676ba2eb2a95ec83060cd0d9a25c
DDongSSik <118751809+DDongSSik@users.noreply.github.com>
2023-06-27 19:04:27 +0900

    적 클래스 구조 생성
    Enemy Class 구조 생성

--- 230615_sds/Assets/Scripts/Enemy.cs
+++ 230615_sds/Assets/Scripts/Enemy.cs
@@ -3,18 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy{
+    public enum EnemyType
+    { 
+        Basic = 0, 
+        Range,
+    }
+
+    private EnemyType enenmyType;
     private string name;
     private int hp;
+    private int atk;
+    private float speed;
 
-    public Enemy(string name, int hp)
+    public Enemy(EnemyType type, string name, int hp, int atk, float speed)
     {
-        this.name = name;
+        this.enenmyType=type;
+        //this.name = name;
         this.hp = hp;   
+        this.atk = atk;
+        this.speed = speed;
     }
 
-    public void GetEnemyInfo()
+    public int Hp
+    {
+        get
+        {
+            return this.hp;
+        }
+    }
+
+    public int Atk
+    {
+        get
+        {
+            return this.atk;
+        }
+    }
+
+    public float Speed
+    {
+        get
+        {
+            return this.speed;
+        }
+    }
+    public bool Hit(int dmg)
+    {
+        if(this.hp - dmg <= 0) return false;
+
+        this.hp -= dmg;
+
+        return true;
+    }
+
+
+    /*public void GetEnemyInfo()
     {
         UnityEngine.Debug.Log($"name : {this.name}  hp : {this.hp}");
-    }
+    }*/
   
 }
